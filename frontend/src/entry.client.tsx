97ea5f464cfef02251f2fe1f5eaa5047b600bc89
kammiii <kamran.nazir2504@gmail.com>
2025-07-24 23:55:34 +0200

    feat: install react-query axios setup query client

--- frontend/src/entry.client.tsx
+++ frontend/src/entry.client.tsx
@@ -1,12 +1,16 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode, startTransition } from 'react';
 import { hydrateRoot } from 'react-dom/client';
 import { HydratedRouter } from 'react-router/dom';
+import { queryClient } from './lib/query-client';
 
 startTransition(() => {
   hydrateRoot(
     document,
     <StrictMode>
-      <HydratedRouter />
+      <QueryClientProvider client={queryClient}>
+        <HydratedRouter />
+      </QueryClientProvider>
     </StrictMode>,
   );
 });
